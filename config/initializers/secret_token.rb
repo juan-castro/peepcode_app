dacb52340e26180abe085fb2a63289f0a4895c19
Juan Castro <j.castro@tukaiz.com>
2012-04-20 20:24:18 -0500

    Started Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeepcodeApp::Application.config.secret_token = 'cd017b100856535035c4e9bcd1d8f18c97e1ea1631644082767e7010cf1ee3076d12f13016282bc01a106a6abddd6cc8a2d151b5c4d569c94b2c44fd37e2c70a'
